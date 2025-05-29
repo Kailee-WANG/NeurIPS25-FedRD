4ec02832478c8e117ad0c48c16faadffd475b562
Kailee-WANG <kacy1.wang@connect.polyu.hk>
2025-05-29 21:10:22 +0800

    FedRD implementation

---
+++ configs/default.py
@@ -0,0 +1,9 @@
+pacs_domain_list = ['p', 'a', 'c', 's']
+domainNet_domain_list = ['c', 'i', 'p', 'q', 'r', 's']
+officehome_domain_list = ['p', 'a', 'c', 'r']
+
+
+log_count_path = './Yourdirectory/FedRD/'
+pacs_path = './Yourdirectory/FedRD/PACS/'
+officehome_path = './Yourdirectory/FedRD/OfficeHome/'
+
